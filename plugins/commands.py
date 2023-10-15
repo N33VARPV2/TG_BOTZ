5ff382bbcca0e3e18e4a38842f3d537572223709
N33VARP <118649748+N33VARPV2@users.noreply.github.com>
2023-10-15 23:03:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -728,7 +728,7 @@ async def settings(client, message):
                     callback_data=f'setgs#max_btn#{settings["max_btn"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    '10' if settings["max_btn"] else f'{MAX_B_TN}',
+                    '𝟷𝟶' if settings["max_btn"] else f'{MAX_B_TN}',
                     callback_data=f'setgs#max_btn#{settings["max_btn"]}#{grp_id}',
                 ),
             ],
