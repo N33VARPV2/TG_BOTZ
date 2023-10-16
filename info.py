cdb9588436b0e8230de3e07fc816ed97305f0fb7
N33VARP <118649748+N33VARPV2@users.noreply.github.com>
2023-10-16 15:49:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,9 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
+QUALITIES = ["360p", "480p", "576p", "720p", "1080p", "1440p", "2160p"]
+
+LANGUAGES = ["malayalam", "tamil" ,"english", "hindi", "telugu", "kannada"]
 
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
 
